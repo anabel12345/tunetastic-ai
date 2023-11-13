5839c1652f767c7b9cfe8224222c25c5a5c7122f
Annabelle <lilacpaw2018@gmail.com>
2023-11-13 11:03:35 -1000

    added code from teachable machine

--- model_utils.js
+++ model_utils.js
@@ -1,57 +1,45 @@
+    // more documentation available at
+    // https://github.com/tensorflow/tfjs-models/tree/master/speech-commands
 
-async function createModel() {
-    const modelURL = 'http://127.0.0.1:5500/model/model.json';
-    const metadataURL = "http://127.0.0.1:5500/model/metadata.json";
+    // the link to your model provided by Teachable Machine export panel
+    const URL = "https://teachablemachine.withgoogle.com/models/IyE2vxLgE/";
 
-    const recognizer = speechCommands.create('BROWSER_FFT', undefined, modelURL, metadataURL);
-    await recognizer.ensureModelLoaded();
-    return recognizer;
-}
+    async function createModel() {
+        const checkpointURL = URL + "model.json"; // model topology
+        const metadataURL = URL + "metadata.json"; // model metadata
 
+        const recognizer = speechCommands.create(
+            "BROWSER_FFT", // fourier transform type, not useful to change
+            undefined, // speech commands vocabulary feature, not useful for your models
+            checkpointURL,
+            metadataURL);
 
+        // check that model and metadata are loaded via HTTPS requests.
+        await recognizer.ensureModelLoaded();
 
-// async function init(render) {
-//     const recognizer = await createModel();
-//     const classLabels = recognizer.wordLabels(); 
-//     console.log(recognizer)
- 
+        return recognizer;
+    }
 
+    async function init() {
+        const recognizer = await createModel();
+        const classLabels = recognizer.wordLabels(); // get class labels
+        const labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < classLabels.length; i++) {
+            labelContainer.appendChild(document.createElement("div"));
+        }
 
+        // listen() takes two arguments:
+        // 1. A callback function that is invoked anytime a word is recognized.
+        // 2. A configuration object with adjustable fields
+        recognizer.listen(result => {
+            const scores = result.scores; // probability of prediction for each class
+            // render the probability scores per class
+            console.log(result)
+        }, {
+            includeSpectrogram: true, // in case listen should return result.spectrogram
+            probabilityThreshold: 0.75,
+            invokeCallbackOnNoiseAndUnknown: true,
+            overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
+        });
 
-//     recognizer.listen(result => {
-//        console.log(result)
-//     }, {
-//         includeSpectrogram: true,
-//         probabilityThreshold: 0.75,
-//         invokeCallbackOnNoiseAndUnknown: true,
-//         overlapFactor: 0.50
-//     });
-    
-
-
-// }
-
-
-
-
-async function init() {
-    const recognizer = await createModel();
-    console.log(recognizer.listen)
-  
-
-    // listen() takes two arguments:
-    // 1. A callback function that is invoked anytime a word is recognized.
-    // 2. A configuration object with adjustable fields
-    recognizer.listen(result => {
-    console.log('hi')
-        const scores = result.scores; // probability of prediction for each class
-        // render the probability scores per class
-     
-    }, {
-        includeSpectrogram: true, // in case listen should return result.spectrogram
-        probabilityThreshold: 0.75,
-        invokeCallbackOnNoiseAndUnknown: true,
-        overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
-    });
-
-}
+    }
