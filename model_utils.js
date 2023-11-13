4217171537688023ff7c17c7853a42980a05a1f2
Annabelle <lilacpaw2018@gmail.com>
2023-11-12 14:36:39 -1000

    created function to make prediction

--- model_utils.js
+++ model_utils.js
@@ -7,3 +7,19 @@ function createModel(){
 
     return recognizer;
 }
+
+async function init(render) {
+    const recognizer = createModel();
+    const classLabels = recognizer.wordLabels(); 
+    
+    recognizer.listen(result => {
+        const scores = result.scores; // probability of prediction for each class
+        let maxScoreIndex;
+        for(let i=0; i<scores.length; i++){
+            if(scores[i]>scores[maxScoreIndex]){
+                maxScoreIndex=i;
+            }
+        }
+        render(classLabels[maxScoreIndex]); //send the prediction to be rendered
+    });
+}
