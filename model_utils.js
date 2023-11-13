f000b4515b0b99c77fd940a2da9310b81a78d035
Annabelle <lilacpaw2018@gmail.com>
2023-11-13 09:02:14 -1000

    added second parameter recognizer.listen

--- model_utils.js
+++ model_utils.js
@@ -1,19 +1,27 @@
-function createModel(){
-    const modelStructureURL = "./model/model.json";
-    const metadataURL = "./model/metadata.json";
 
-    //Define audio input
-    const recognizer = speechCommands.create('BROWSER_FFT')
+async function createModel() {
+    const modelURL = 'https://anabel12345.github.io/tunetastic-ai/model/model.json';
+    const metadataURL = 'https://anabel12345.github.io/tunetastic-ai/model/metadata.json';
 
+    const recognizer = speechCommands.create('BROWSER_FFT', undefined, modelURL, metadataURL);
+    await recognizer.ensureModelLoaded();
     return recognizer;
 }
 
+// Rest of your code...
+
+
 async function init(render) {
-    const recognizer = createModel();
+    const recognizer = await createModel();
     const classLabels = recognizer.wordLabels(); 
-    
-    recognizer.listen(result => {
+    console.log(recognizer)
+ 
+
+
+
+   recognizer.listen(result => {
         const scores = result.scores; // probability of prediction for each class
+        console.log(scores)
         let maxScoreIndex;
         for(let i=0; i<scores.length; i++){
             if(scores[i]>scores[maxScoreIndex]){
@@ -21,5 +29,13 @@ async function init(render) {
             }
         }
         render(classLabels[maxScoreIndex]); //send the prediction to be rendered
-    });
-}
+    }, {
+        includeSpectrogram: true,
+        probabilityThreshold: 0.75,
+        invokeCallbackOnNoiseAndUnknown: true,
+        overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
+  
+      });
+
+
+    }
--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 dropDown(hamburger, navLinks);
+
+document.querySelector('#start').addEventListener('click', ()=>{
+    init(displayData)
+});
